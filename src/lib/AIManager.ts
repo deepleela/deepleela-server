ba0fdaf4aab85de4aa42f83cf1cdb3c46837d39f
deepleela <leelazero@protonmail.com>
2018-04-29 20:36:32 +0800

    fix ai is null

--- src/lib/AIManager.ts
+++ src/lib/AIManager.ts
@@ -16,17 +16,25 @@ export default class AIManager {
     private static readonly controllers = new Set<Controller>();
 
     static createController(engine: string = 'leela') {
+        
+        engine = engine ? engine.toLowerCase() : 'leela';
         if (AIManager.controllers.size >= AIManager.maxInstances) return null;
         if (!AIManager.configs.has(engine)) return null;
 
         let leelaConfigs = AIManager.configs.get('leela');
-        let leelaArgs = ['--gtp', '--noponder', '--playouts', `${leelaConfigs ? leelaConfigs.playouts || 1000 : 1000}`,];
+        let leelaArgs = ['--gtp', '--noponder'];
+        if (leelaConfigs) {
+            if (leelaConfigs.playouts) leelaArgs.push('--playouts', `${leelaConfigs ? leelaConfigs.playouts || 1000 : 1000}`);
+        }
 
         let leelazeroConfigs = AIManager.configs.get('leelazero');
-        let leelazeroArgs = ['--gtp', '--noponder', '--playouts', `${leelazeroConfigs ? leelazeroConfigs.playouts || 2000 : 2000}`, '-w', leelaConfigs.weights];
-        
-        let argsMap = new Map([['leela', leelaArgs], ['leelazero', leelazeroArgs]]);
+        let leelazeroArgs = ['--gtp', '--noponder', '-w', leelazeroConfigs.weights];
+        if (leelazeroConfigs) {
+            if (leelazeroConfigs.playouts) leelazeroArgs.push('--playouts', `${leelazeroConfigs ? leelazeroConfigs.playouts || 2000 : 2000}`);
+        }
 
+        let argsMap = new Map([['leela', leelaArgs], ['leelazero', leelazeroArgs]]);
+        console.log(argsMap.get(engine));
         let ai = new Controller(AIManager.configs.get(engine).exec, argsMap.get(engine) || []);
         AIManager.controllers.add(ai);
 
@@ -34,6 +42,7 @@ export default class AIManager {
     }
 
     static async releaseController(controller: Controller) {
+        if (!controller) return;
         await controller.stop();
         AIManager.controllers.delete(controller);
     }
--- src/lib/GoServer.ts
+++ src/lib/GoServer.ts
@@ -9,22 +9,23 @@ export default class GoServer extends EventEmitter {
     private client: WebSocket;
     private keepaliveTimer: NodeJS.Timer;
     private ai: Controller;
+    private engine: string;
     private sysHanlders: Map<string, Function>;
 
     constructor(client: WebSocket) {
         super();
         this.client = client;
 
-        this.client.on('message', this.handleMessage.bind(this));
-        this.client.on('close', this.handleClose.bind(this));
-        this.client.on('error', this.handleError.bind(this));
+        this.client.on('message', this.handleMessage);
+        this.client.on('close', this.handleClose);
+        this.client.on('error', this.handleError);
 
         this.keepaliveTimer = setInterval(() => this.client.ping(), 15 * 1000);
 
         this.sysHanlders = new Map([[Protocol.sys.requestAI, this.handleRequestAI]]);
     }
 
-    private handleMessage(data: WebSocket.Data) {
+    private handleMessage = (data: WebSocket.Data) => {
         let msg: ProtocolDef = null;
 
         try {
@@ -49,11 +50,11 @@ export default class GoServer extends EventEmitter {
         }
     }
 
-    private handleClose(code: number, reason: string) {
+    private handleClose = (code: number, reason: string) => {
         this.close();
     }
 
-    private handleError(error: Error) {
+    private handleError = (error: Error) => {
         this.close();
     }
 
@@ -90,10 +91,13 @@ export default class GoServer extends EventEmitter {
     }
 
     private handleRequestAI = (cmd: Command) => {
-        if (this.ai && this.ai.process) {
+        if (this.ai && this.ai.process && cmd.args === this.engine) {
             this.sendSysResponse({ id: cmd.id, name: cmd.name, args: [true, 0] });
             return;
         }
+
+        AIManager.releaseController(this.ai);
+        this.ai = null;
 
         let ai = AIManager.createController(cmd.args);
 
@@ -103,11 +107,11 @@ export default class GoServer extends EventEmitter {
             return;
         }
 
-        ai.on('stopped', (args) => { AIManager.releaseController(ai), this.ai = null, console.log(args) });
+        ai.on('stopped', (args) => { AIManager.releaseController(ai), console.error(cmd.args, args) });
         ai.start();
 
         this.ai = ai;
-
+        this.engine = ai.process != null ? cmd.args : undefined;
         this.sendSysResponse({ id: cmd.id, name: cmd.name, args: [ai.process != null, 0] });
     }
 
